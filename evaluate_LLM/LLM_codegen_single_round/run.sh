f95a8d8054015ca54f4729976178397437089cfd
j <j@wj2ai.com>
2025-07-27 15:25:40 +0800

    recommand 8192

--- evaluate_LLM/LLM_codegen_single_round/run.sh
+++ evaluate_LLM/LLM_codegen_single_round/run.sh
@@ -5,7 +5,7 @@ python generate_base_inst.py  ../data/prompts/buggy_errmsg/single_function_allin
         --bs 4 \
         --temperature 0.2 \
         --n_samples 200 \
-        --resume --max-len 4096 \
+        --resume --max-len 8192 \
         --backend vllm 
 
 
@@ -16,7 +16,7 @@ python generate_base_inst.py  ../data/prompts/buggy_errmsg/single_function_allin
         --bs 4 \
         --temperature 0.8 \
         --n_samples 200 \
-        --resume --max-len 4096 \
+        --resume --max-len 8192 \
         --backend vllm 
 
 
@@ -24,7 +24,7 @@ python generate_base_inst.py  ../data/prompts/buggy_errmsg/single_function_allin
 python generate_base_inst.py  ../data/prompts/buggy_errmsg/single_function_allinone.saved.jsonl \
         --model deepseek-ai/deepseek-coder-6.7b-base \
         --greedy \
-        --resume --max-len 4096 \
+        --resume --max-len 8192 \
         --backend vllm 
 
 
