bb8aaab90acd8f585331bfd60551f6ad1dbbe358
taicen <taicen@wj2ai.com>
2026-03-11 17:34:24 +0800

    f

--- defectsc_tpl/run_web.sh
+++ defectsc_tpl/run_web.sh
@@ -3,9 +3,9 @@ set -euo pipefail
 
 
 source /src/.venv/bin/activate
-exec /src/.venv/bin/python -m gunicorn defects4c_api:app \
-     --worker-class uvicorn.workers.UvicornWorker \
-     --bind 0.0.0.0:80
+#exec /src/.venv/bin/python -m gunicorn defects4c_api:app \
+#     --worker-class uvicorn.workers.UvicornWorker \
+#     --bind 0.0.0.0:80
 
 
-gunicorn -k uvicorn.workers.UvicornWorker --workers 8 --bind 0.0.0.0:80 main:app --timeout 600
+gunicorn -k uvicorn.workers.UvicornWorker --workers 8 --bind 0.0.0.0:80 new_main:app --timeout 600
